df328ce110fc04096e6e831bad64a0444e81a68b
Gaurav Pratap Singh <gauravpratapsingh321@gmail.com>
2025-04-17 20:07:10 +0530

    commit

--- twitter.sol
+++ twitter.sol
@@ -87,6 +87,75 @@ contract TweetContract {
         //abc - def,ghi,opr
         following[msg.sender].push(_followed);
     }
+    function allow(address _operator) public {
+        operators[msg.sender][_operator] = true;
+    }
 
+    function disallow(address _operator) public {
+        operators[msg.sender][_operator] = false;
+    }
+
+    function getLatestTweets(uint256 count)
+        public
+        view
+        returns (Tweet[] memory)
+    {
+        require(count > 0 && count <= nextId, "Count is not proper");
+
+        Tweet[] memory _tweets = new Tweet[](count); //array length - count
+
+        uint256 j;
+
+        for (uint256 i = nextId - count; i < nextId; i++) {
+            //count = 5 nextId=7; 7-5=2,3,4,5,6
+            Tweet storage _structure = tweets[i];
+
+            _tweets[j] = Tweet(
+                _structure.id,
+                _structure.author,
+                _structure.content,
+                _structure.createdAt
+            );
+
+            j = j + 1;
+        }
+
+        return _tweets;
+    }
+
+    function getLatestofUser(address _user, uint256 count)
+        public
+        view
+        returns (Tweet[] memory)
+    {
+        //7
+        Tweet[] memory _tweets = new Tweet[](count); //new memory array whoose length is count
+
+        //tweetsOf[_user] is having all the id's of the user
+
+        uint256[] memory ids = tweetsOf[_user];
+
+        ///ids is an array
+
+        require(count > 0 && count <= ids.length, "Count is not defined");
+
+        uint256 j;
+
+        for (uint256 i = ids.length - count; i < ids.length; i++) {
+            //5-3 = 2 to 5
+            Tweet storage _structure = tweets[ids[i]]; //i=2 id[2]=15 tweets[15]
+
+            _tweets[j] = Tweet(
+                _structure.id,
+                _structure.author,
+                _structure.content,
+                _structure.createdAt
+            );
+
+            j = j + 1;
+        }
+
+        return _tweets;
+    }
 
 }
